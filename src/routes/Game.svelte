37a750ce662bbcb8b03c37b7c219d1e28c2c286d
unknown <vitali.shuman96@gmail.com>
2024-04-26 17:56:47 +0300

    Adds initial ui structure

---
+++ src/routes/Game.svelte
@@ -0,0 +1,30 @@
+<div class="game">
+	<div class="info"></div>
+	<div class="grid-container"></div>
+	<div class="info"></div>
+</div>
+
+<style>
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+	}
+
+	.info {
+		width: 80vmin;
+		height: 10vmin;
+		background-color: red;
+	}
+
+	.grid-container {
+		display: grid;
+		grid-template-columns: auto auto auto;
+		background-color: #2196f3;
+		/* padding: 10px; */
+		width: 80vmin;
+		height: 80vmin;
+	}
+</style>
